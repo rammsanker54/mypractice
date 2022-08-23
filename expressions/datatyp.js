2d52d46dc550c308366e380a9886ddd8c22044cb
rammsanker <109334253+rammsanker54@users.noreply.github.com>
2022-08-23 21:22:07 +0530

    Add files via upload

---
+++ expressions/datatyp.js
@@ -0,0 +1,158 @@
+var x=10;
+var y='status';
+var z="";
+var z=[1,2,3,4]
+
+console.log(1>2 && 1<2
+
+
+
+
+
+var x=2;
+if(x>21){ console.log("eligible for marriage")}
+else{
+console.log(" not eligible for marriage")
+}
+
+
+
+// nested if
+
+
+var x=22;
+if(x>21){
+	var gender="female";
+
+
+	if(gender=="female"){
+		console.log("eligible for marriage")
+	}
+	else{
+console.log(" not eligible for marriage")
+ }
+}
+else{
+console.log(" not eligible for marriage")
+}
+
+
+
+
+
+
+var a=10;
+switch (a){
+	case 0:
+	console.log("zero")
+	case 1:
+	console.log("one")
+	case 10:
+	console.log("ten")
+}
+
+
+
+
+
+
+
+// addition
+var x=20;
+var y=20;
+var z=x+y;
+console.log(z);
+//  subraction
+
+console.log(z=x-y);
+//  multiplication
+
+console.log(z=x*y);
+// divide
+
+console.log(z=x/y);
+
+
+//  calculate Area of Circle
+// let dia of circle=50mm
+var r=25;
+var Area= Math.PI* r**2;
+console.log(Area);
+
+
+// calculate Area of  Triangle
+// let height= 20mm, base=10mm
+var height=20;
+var base=10;
+var Area =  (height*base)/2
+console.log(Area);
+
+
+// Calculating Simple Interest
+// let Principal Amount=10000/-
+//  Rate of Interest per year in decimal; r = R/100
+//  Rate of Interest per year as a percent; R =  10%
+//  Time Periods in 6 yrs
+var P=10000;
+var r=R;
+var t=6;
+var Interest = P*r*t;
+console.log(Interest
+
+
+
+
+
+// Calculating Square
+var r=25;
+var square=Math.pow(r,2);
+console.log(square);
+
+
+//  calculate total and average
+var a=10;
+var b=20;
+var c=30;
+var total = a+b+c;
+var average =(a+b+c)/3;
+console.log(total );
+console.log(average );
+
+
+// check number is positive or not
+var number=-1
+if(number>0){console.log("positive")}
+else{console.log("not")}
+
+
+// Number even or odd
+var array=11;
+if(array%2!=0)
+{console.log("odd");}
+else{console.log("even");}
+
+//  check whether number is divisible by 5
+var check=15;
+if(check%5==0)
+{console.log("number is divisible by 5");}
+else{console.log("number is not divisible by 5");}
+
+
+// Print grade based on marks of a student
+var mani= 85;
+var ram =75;
+
+switch (mani,ram){
+	case mani>80 :
+    case ram>80  :
+	console.log("a")
+
+	case mani>70 :
+        case ram>70  :
+	console.log("b")
+}
+	
+  	
+	
+	
+	
---
+++ expressions/js/script.js
@@ -0,0 +1,92 @@
+// addition
+var x=20;
+var y=20;
+var z=x+y;
+console.log(z);
+// 2 subraction
+
+console.log(z=x-y);
+// 3 multiplication
+
+console.log(z=x*y);
+// divide
+
+console.log(z=x/y);
+
+
+//  calculate Area of Circle 
+// let dia of circle=50mm
+var r=25;
+var Area= Math.PI* r**2;
+console.log(Area);
+// calculate Area of  Triangle
+// let height= 20mm, base=10mm
+var height=20;
+var base=10;
+var Area =  (height*base)/2
+console.log(Area);
+// Calculating Simple Interest
+// let Principal Amount=10000/-
+//  Rate of Interest per year in decimal; r = R/100
+//  Rate of Interest per year as a percent; R =  10%
+//  Time Periods in 6 yrs
+var P=10000;
+var r=0.1;
+var t=6;
+var Interest = P*r*t;
+console.log(Interest);
+// Calculating Square
+var r=25;
+var square=r**2;
+console.log(square);
+//  calculate total and average
+var a=10;
+var b=20;
+var c=30;
+var total = a+b+c;
+var average =(a+b+c)/3;
+console.log(total );
+console.log(average );
+
+
+// check number is positive or not
+var number=-1
+if(number>0){console.log("positive");}
+else{console.log("not");}
+
+
+// Number even or odd
+var array=11;
+if(array%2==0)
+{console.log("odd");}
+else{console.log("even");}
+
+//  check whether number is divisible by 5
+var check=15;
+if(check%5==0)
+{console.log("number is divisible by 5");}
+else{console.log("number is not divisible by 5");}
+
+
+// Print grade based on marks of a student
+var mani= 85;
+var ram =75;
+
+switch (mani,ram){
+	case mani>80 :
+    case ram>80  :
+	console.log("a")
+
+	case mani>70 :
+        case ram>70  :
+	console.log("b")
+}
+	
+	
+	
+	
+
+
+
+
+
